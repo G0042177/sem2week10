60e0f9e8c188058af41b865111c521d4922451e4
G00421777@atu.ie <G00421777@atu.ie>
2024-04-10 17:11:46 +0100

    g

---
+++ src/test/java/ie/atu/calTest.java
@@ -0,0 +1,17 @@
+package ie.atu;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Running CalTest")
+public class calTest {
+@BeforeAll
+    static void init()
+{
+    System.out.println("Beginning of Test");
+}
+
+
+
+}
